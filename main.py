109430cc2f157356f9e4fd24aaf6e4325a3cb69e
AKSHAY RATHEESH <92791421+AkshayRatheesh@users.noreply.github.com>
2023-04-03 23:19:42 +0530

    solved errors and most of the edge cases

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ user_search = input('The word you want to search : ').lower()
 
 
 def translate(words):
+    print(words)
     if words in data:
-        return data[user_search] # searching word and return meaning
+        return data[words] 
+        # searching word and return meaning
     else:       
-        sugget_words = get_close_matches(user_search, data.keys()) # word suggetions
+        sugget_words = get_close_matches(words, data.keys())
+        # word suggetions
         
         if len(sugget_words) > 0 :
-            #error fixing
-            user_searchs = input(f"Oops! no word match, do you mean this {sugget_words} : ").lower()
-            return translate(user_searchs)
-            #error fixing
+            second_search = input(f"Oops! no word match, do you mean this {sugget_words} :\n").lower()
+            #reading suggested word input
+                        
+            if second_search in sugget_words and second_search != words:
+                return data[second_search]
+                #second in the suggested list that means word meaning available
+            elif second_search == words:
+                return "no matching found, try again"
+                #same word try to search again
+            else:        
+                return translate(second_search)
+                #Recursion -- second searched word new and not in the suggested list
+        
         else:        
             return "no matching found, try again"
+            #word not in the file
 
 print(translate(user_search))
-
